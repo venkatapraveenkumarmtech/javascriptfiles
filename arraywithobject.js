0929f1ca5bc9c3ab5fe2cb579a7015277bc205e1
praveen <venkatapraveenkumarmtech@gmail>
2025-05-17 12:30:21 +0530

    functions

---
+++ arraywithobject.js
@@ -0,0 +1,19 @@
+let colleges=[
+    {
+        Name:"deepthi junior college",
+        group:["mpc","bipc","cec","hec"],
+        noofstudents:3000
+    },
+    {
+        Name:"Bavana junior college",
+        group:["mpc","bipc","hec"],
+        noofstudents:2000
+    },
+    {
+        Name:"abays junior college",
+        group:["mpc","bipc"],
+        noofstudents:1000
+    }
+]
+
+console.log(colleges[2].Name);
---
+++ functions.js
@@ -0,0 +1,8 @@
+let course=function()
+{
+    console.log("javascript");
+    console.log("html");
+    console.log("css");
+}
+console.log(course);
+console.dir(course);
