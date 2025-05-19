1fadbe8d0e8ebf2344d9b8dacc423f7ce0d9519f
praveen <venkatapraveenkumarmtech@gmail>
2025-05-19 20:32:53 +0530

    functions-1

---
+++ functions1.js
@@ -0,0 +1,9 @@
+function multiple(num1,num2)
+{
+    result=num1*num2
+    return result
+}
+
+console.log(multiple(4,6));
+document.write("functions are mainly used for reusability<br>");
+document.write(multiple(4,6));
