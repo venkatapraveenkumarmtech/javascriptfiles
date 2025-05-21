3bf991ef33e02fafdca91e6216c760431a1c8ddd
praveen <venkatapraveenkumarmtech@gmail>
2025-05-21 20:43:54 +0530

    functiontypesorder

--- colorfunction.js
+++ colorfunction.js
@@ -1,24 +1,28 @@
-// function blue()
-// {
-// console.log("%c  ","color:orange");
+function blue()
+{ 
+    console.log("%c       ","background-color:orange");
 
-// console.log("%c  ","color:black");
-// }
+   console.log("%c  ","background-color:black");
+ }
 
-function firstsection()
-{
-    console.log("%c  ","color:purple");
-    console.log("%c   ","color:white");
-    blue() 
+ function firstsection()
+ {
+     console.log("%c  ","background-color:purple");
+     console.log("%c   ","background-color:white");
+    blue();
 
 
-}
+ }
+ firstsection();
 
-function secondsection()
-{
-    console.log("%c  ","color:green");
-    console.log("%c  ","color:darkgrey");
-}
+ function secondsection()
+ {
+     console.log("%c  ","background-color:green");
+     console.log("%c  ","background-color:darkgrey");
+     blue();
+     firstsection();
+ }
+ secondsection();
 
 
 
---
+++ functiontypes.js
@@ -0,0 +1,29 @@
+//named function
+function display()
+{
+    console.log("this is named function");
+}
+display();
+//anonymus function: in this function doesnt have any name
+
+let show=function()
+{
+    console.log("this is anonymus function");
+}
+show();
+//IIFE : immidiate invoked function expression
+(function()
+{
+    console.log("this is IIFE function");
+})();
+//arrow function:
+let arrow=() =>{
+    console.log("this is arrow function");
+}
+arrow();
+
+
+
+
+
+
