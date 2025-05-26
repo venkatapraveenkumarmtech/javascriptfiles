6eb7d17de64fc688f81fffa3300e13de10345974
praveen <venkatapraveenkumarmtech@gmail>
2025-05-26 18:02:43 +0530

    memeber and this

---
+++ callback1.js
@@ -0,0 +1,14 @@
+function misard(excellent,name)
+{
+    console.log("hello",name);
+    excellent();
+}
+
+
+function excellent()
+{
+    console.log("congratulations mr");
+
+}
+
+misard(excellent,"praveen");
---
+++ members.js
@@ -0,0 +1,14 @@
+let movie={
+    hero1: "ntr",
+    hero2: "rc",
+    director:"rm",
+    directormovies:["stno1","magadeera","simhadri","rrr","eega"],
+    heroinename:function(){
+        console.log("always heroine name is bindu");
+    }
+}
+document.write(movie.hero2);
+document.write(movie.hero1);
+let hi=movie.heroinename();
+hi();
+
