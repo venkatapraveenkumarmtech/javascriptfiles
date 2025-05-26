0083e26ad693ed176910f318099706512e1c4abe
praveen <venkatapraveenkumarmtech@gmail>
2025-05-26 20:14:13 +0530

    thistypekeyword

---
+++ this.js
@@ -0,0 +1,28 @@
+// var sports=["hockey","tennies","football","cricket","batmenton"];
+// var tennies=["aus","french","wimbuldon","us"];
+// console.log(this);
+// let fedarar={
+//     name:"jamesFedarer",
+//     age:45,
+//     country:"swis",
+//     relatedsport:"tennies",
+//     medals:["aus","french","wimbuldon","us"],
+//     achivment:function(){
+//         console.log("fedarer",this);
+//     }
+// }
+
+// let fed=fedarar.achivment();
+// console.log(fed);
+function spoartsdetails()
+{
+    type="outdoor",
+    members=11,
+    tennies=4,
+    series=["aus","french","wimbuldon","us"],
+
+    console.log(this);
+
+}
+let sport=new spoartsdetails();
+console.log(sport);
