d5defacee5e0266e2486d5eb92b13f89cbc9417b
praveen <venkatapraveenkumarmtech@gmail>
2025-06-02 18:18:51 +0530

    conditional

---
+++ conditional statements/conditinal.js
@@ -0,0 +1,36 @@
+// let userage=25
+// if(userage>=18)
+// {
+//     console.log("this person is eligible for voting");
+// }
+// else{
+//     console.log("this person is not eligible for voting");
+// }
+// let num=prompt("enter your number:")
+// if(num%2==0)
+// {
+//     console.log("this number is evennumber");
+// }
+// else{
+//     console.log("this is odd number");
+// }
+const username="venkatesh";
+const userpassword="1234";
+
+let name="surendra";
+let password="1234";
+if (name==username)
+{
+    if(password==userpassword)
+    {
+        console.log('login success');
+    }
+    else{
+        console.log('incorrect password');
+    }
+    console.log("user name is correct");
+}
+else{
+    console.log("username is not correct");
+}
+
