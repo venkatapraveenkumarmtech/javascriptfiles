8e8c682e9f4314d8d98ea4a7459c8d39d97b6efc
praveen <venkatapraveenkumarmtech@gmail>
2025-06-03 20:08:36 +0530

    errorhandaling

---
+++ error.js
@@ -0,0 +1,22 @@
+// try{
+//     const a=10;
+//     a=30;
+    
+
+// }
+// catch(error)
+// {
+// console.log(error);
+// }
+try{
+    
+    var b=8;
+    var c=a*b;
+
+}
+catch(error){
+    console.log("our error is like it:",name.error);
+}
+finally{
+    console.log("programme is excutted");
+}
