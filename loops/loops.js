4262d4e930df412004f598fbfc500e27e1ff328b
praveen <venkatapraveenkumarmtech@gmail>
2025-06-04 22:01:41 +0530

    loops

---
+++ loops/loops.js
@@ -0,0 +1,59 @@
+// let games=[
+//     {game1:"cricket"},
+//     {game2: "football"},
+//     {game3:"vollyball"},
+//     {game4:"tennies"},
+//     {game5:"batmenton"},
+//     {game6:"chess"},
+//     {game7:"carems"}
+// ]
+// for(var game of games)
+// {
+//    console.log(game);
+//     // if(game="vollyball")
+//     // {
+//     //    console.log("I know this game how play");
+//     // }
+// }   
+/*var books=['Thewhitetiger','wingsoffire','halfgirlfriend','secreat']
+for(var novels of books)
+{
+    console.log(novels);
+}*/
+// let pro={
+//     firstname:"sujan",
+//     lastname:"yadav",
+//     age:26,
+//     city:"hyd",
+//     mobile:"9000-6777"
+// }
+// for(let dude in pro)
+// {
+//     console.log(dude);
+// }
+// for(i=1;i<=5;i++)
+// {
+//     console.log("we will get the job at any cost:");
+// }
+// let course=["js","html","css","c#",".netcore"]
+// for(let i=0;i<course.length;i++)
+// {
+//     console.log(course[i]);
+// }
+//while:-
+// let i=0;
+// while(i<=7)
+// {
+//     console.log(i);
+//     i++
+// }
+
+
+//do-while:-
+
+let i=10;
+do{
+    console.log(i);
+    i++;
+}
+while(i<=5)
